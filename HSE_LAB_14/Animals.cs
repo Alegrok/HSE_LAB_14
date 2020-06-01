90ed732771b25999741fb02f5519eded0e5d67dd
Александр Классен <aiklassen@edu.hse.ru>
2020-06-01 19:17:33 +0500

    Добавьте файлы проекта.

---
+++ HSE_LAB_14/Animals.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HSE_LAB_14
+{
+    public interface IExecutable
+    {
+        public abstract void Show();
+
+        public abstract Animals CreateObjectAnimals();
+
+        public abstract Animals CreateObjectAnimalsRandom();
+
+        public abstract int InputInt();
+
+        public abstract int InputInt(int min, int max);
+
+        public abstract string InputString();
+    }
+
+    public class Animals : IExecutable, IComparable, ICloneable
+    {
+        protected Random random = new Random();
+        protected string[] arrayNames = {"Жучка", "Синий", "Снежок", "Дакар", "Летяга", "Шарик", "Жвачка", "Дэнни", "Тайсон", "Джек" };
+        protected string[] arrayKinds = { "Собака", "Кошка", "Лис", "Синица", "Корова", "Воробей", "Мышь", "Конь", "Хомяк", "Дельфин" };
+        string name;
+        string kind;
+        int weight;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != value.Replace(" ", "")) name = "Неизвестный";
+                else name = value;
+            }
+        }
+
+        public string Kind
+        {
+            get { return kind; }
+            set
+            {
+                if (value != value.Replace(" ", "")) kind = "Неизвестный";
+                else kind = value;
+            }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value <= 10000 && value >= 1) weight = value;
+                else weight = 1;
+            }
+        }
+
+        public Animals()
+        {
+            Name = "Неизвестный";
+            Kind = "Неизвестный";
+            Weight = 1;
+        }
+
+        public Animals(string name, string kind, int weight)
+        {
+            Name = name;
+            Kind = kind;
+            Weight = weight;
+        }
+
+        public Animals CreateObjectAnimals()
+        {
+            Console.WriteLine("\nВведите имя");
+            string name = InputString();
+            Console.WriteLine("\nВведите вид");
+            string kind = InputString();
+            Console.WriteLine("\nВведите вес");
+            int weight = InputInt(1, 10000);
+            return new Animals(name, kind, weight);
+        }
+
+        public Animals CreateObjectAnimalsRandom()
+        {
+            string name = arrayNames[random.Next(0, arrayNames.Length)];
+            string kind = arrayKinds[random.Next(0, arrayKinds.Length)];
+            int weight = random.Next(1, 10000);
+            return new Animals(name, kind, weight);
+        }
+
+        virtual public void Show()
+        {
+            Console.WriteLine("Имя: " + Name);
+            Console.WriteLine("Вид: " + Kind);
+            Console.WriteLine("Вес = " + Weight);
+        }
+
+        public int CompareTo(object obj)
+        {
+            Animals animals = (Animals)obj;
+            if (string.Compare(Name, animals.Name) > 0) return 1;
+            else if (string.Compare(Name, animals.Name) < 0) return -1;
+            else return 0;
+        }
+
+        public Animals ShallowCopy()
+        {
+            return (Animals)MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            return new Animals(Name, Kind, Weight);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Animals animal = (Animals)obj;
+            return Name.Equals(animal.Name) & Kind.Equals(animal.Kind) & Weight == animal.Weight;
+        }
+
+        public string InputString()
+        {
+            string str;
+            bool inputCheck;
+            do
+            {
+                Console.Write("\nВвод: ");
+                str = Console.ReadLine();
+                inputCheck = str != "";
+                if (!inputCheck) Console.WriteLine("Ошибка ввода! Введите не пустые данные\n");
+            } while (!inputCheck);
+            return str;
+        }
+
+        public int InputInt()
+        {
+            int number;
+            bool inputCheck;
+            do
+            {
+                Console.Write("\nВвод: ");
+                inputCheck = int.TryParse(Console.ReadLine(), out number);
+                if (!inputCheck) Console.WriteLine("Ошибка ввода! Введите целое число\n");
+            } while (!inputCheck);
+            return number;
+        }
+
+        public int InputInt(int min, int max)
+        {
+            int number;
+            bool inputCheck;
+            do
+            {
+                Console.Write("\nВвод: ");
+                inputCheck = int.TryParse(Console.ReadLine(), out number) && number >= min && number <= max;
+                if (!inputCheck) Console.WriteLine("Ошибка ввода! Введите целое число в пределах от {0} до {1} (включительно)\n", min, max);
+            } while (!inputCheck);
+            return number;
+        }
+
+        public override string ToString()
+        {
+            return name + " " +  kind + " " + weight;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Kind, Weight);
+        }
+    }
+
+    public class AnimalsComparer : IComparer<Animals>
+    {
+        public int Compare(Animals animal1, Animals animal2)
+        {
+            if (string.Compare(animal1.Kind, animal2.Kind) > 0) return 1;
+            else if (string.Compare(animal1.Kind, animal2.Kind) < 0) return -1;
+            else return 0;
+        }
+    }
+}
---
+++ HSE_LAB_14/Program.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HSE_LAB_14
+{
+    public class Program
+    {
+        private const uint UINT_MIN_VALUE = 1;
+        private const uint UINT_MAX_VALUE = 100;
+        private const int INT_MIN_VALUE = 0;
+        private const int INT_MAX_VALUE = 7;
+        private static List<List<Animals>> list = new List<List<Animals>>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в приложение по работе с LINQ запросами");
+            while (true)
+            {
+                Console.WriteLine("\nМеню приложения:");
+                Console.WriteLine("1 - Создание коллекции с коллекциями");
+                Console.WriteLine("2 - Печать коллекции");
+                Console.WriteLine("3 - Выборка данных");
+                Console.WriteLine("4 - Получение счетчика");
+                Console.WriteLine("5 - Использование операций над множествами");
+                Console.WriteLine("6 - Агрегирование данных");
+                Console.WriteLine("7 - Группировка данных");
+                Console.WriteLine("0 - Выход из приложения");
+
+                int choice = InputInt();
+
+                if (choice == 0)
+                {
+                    Console.WriteLine("\n0 - Выход из приложения");
+                    Console.WriteLine("Завершение работы в приложении по работе с LINQ запросами");
+                    break;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        CreateCollection();
+                        break;
+                    case 2:
+                        PrintCollection();
+                        break;
+                    case 3:
+                        DataSampling();
+                        break;
+                    case 4:
+                        GetCounter();
+                        break;
+                    case 5:
+                        OperationsOnSets();
+                        break;
+                    case 6:
+                        DataAggregation();
+                        break;
+                    case 7:
+                        DataGrouping();
+                        break;
+                }
+            }
+        }
+
+        private static void CreateCollection()
+        {
+            Console.WriteLine("\n1 - Создание коллекции с коллекциями");
+
+            Console.WriteLine("Введите размер коллекции для ее создания");
+            uint size = InputUint();
+
+            list = CreateList(size);
+            Console.WriteLine("Коллекция успешно создана");
+
+            Console.WriteLine("\nСоздание коллекции с коллекциями завершено");
+        }
+
+        public static List<List<Animals>> CreateList(uint size)
+        {
+
+            var list = new List<List<Animals>>();
+            Animals animal = new Animals();
+
+            for (int i = 0; i < 4; i++)
+            {
+                List<Animals> newList = new List<Animals>();
+
+                for (int j = 0; j < size; j++)
+                    newList.Add((Animals)animal.CreateObjectAnimalsRandom().Clone());
+
+                list.Add(new List<Animals>(newList));
+            }
+            return list;
+        }
+
+        public static void PrintCollection()
+        {
+            Console.WriteLine("\n2 - Печать коллекции");
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекция пустая");
+                return;
+            }
+
+            int order = 1;
+            list.ForEach((l) =>
+            {
+                Console.WriteLine($"\nКоллекция {order}:");
+                l.ForEach(e => Console.WriteLine(e));
+                order++;
+            });
+            Console.WriteLine("\nПечать коллекции успешно завершена");
+        }
+
+        private static void DataSampling()
+        {
+            Console.WriteLine("\n3 - Выборка данных");
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекция пустая");
+                Console.WriteLine("Выборка данных не завершена");
+                return;
+            }
+
+            Console.WriteLine("Введите имя для поиска элементов коллекции");
+            string nameSearch = InputString();
+
+            var animalsSearch1 = Query.SearchAnimals(list, nameSearch);
+            var animalsSearch2 = Methods.SearchAnimals(list, nameSearch);
+
+            Console.WriteLine("\nНайдены следующие элементы:");
+            if (animalsSearch1.Count != 0)
+                animalsSearch1.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nНайдены следующие элементы:");
+            if (animalsSearch2.Count != 0)
+                animalsSearch2.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nВыборка данных успешно завершена");
+        }
+
+        private static void GetCounter()
+        {
+            Console.WriteLine("\n4 - Получение счетчика");
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекции пусты");
+                Console.WriteLine("Получение счетчика не завершено");
+                return;
+            }
+
+            Console.WriteLine("Введите имя для подсчета элементов коллекции");
+            string nameCount = InputString();
+
+            var animalsCount1 = Query.CountAnimals(list, nameCount);
+            var animalsCount2 = Methods.CountAnimals(list, nameCount);
+
+            Console.WriteLine($"\nКол-во найденных элементов: {animalsCount1}");
+            Console.WriteLine($"Кол-во найденных элементов: {animalsCount2}");
+
+            Console.WriteLine("\nПолучение счетчика успешно завершено");
+        }        
+
+        private static void OperationsOnSets()
+        {
+            Console.WriteLine("\n5 - Использование операций над множествами");
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекции пусты");
+                Console.WriteLine("Использование операций над множествами не завершено");
+                return;
+            }
+
+            Console.WriteLine("Найдем имена животных, которые есть в 1 коллекции, но нет во 2 коллекции");
+            var animalsExpect1 = Query.ExpectAnimals(list);
+            var animalsExpect2 = Methods.ExpectAnimals(list);
+
+            Console.WriteLine("\nИмена, которые есть в 1 коллекции, но нет во 2 коллекции:");
+            if (animalsExpect1.Count != 0)
+                animalsExpect1.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nИмена, которые есть в 1 коллекции, но нет во 2 коллекции:");
+            if (animalsExpect2.Count != 0)
+                animalsExpect2.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nТеперь объединим 1 коллекцию со 2 коллекцией");
+
+            var animalsUnion1 = Query.UnionAnimals(list);
+            var animalsUnion2 = Methods.UnionAnimals(list);
+
+            Console.WriteLine("\nОбъединение 1 коллекции и 2 коллекции:");
+            if (animalsUnion1.Count != 0)
+                animalsUnion1.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Коллекции пусты");
+
+            Console.WriteLine("\nОбъединение коллекции 1 и коллекции 2:");
+            if (animalsUnion2.Count != 0)
+                animalsUnion2.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Коллекции пусты");
+
+            Console.WriteLine("\nА сейчас найдем имена, которые присутствуют в обоих коллекциях");
+
+            var animalsIntersect1 = Query.IntersectAnimals(list);
+            var animalsIntersect2 = Methods.IntersectAnimals(list);
+
+            Console.WriteLine("\nИмена, которые есть в обоих коллекциях:");
+            if (animalsIntersect1.Count != 0)
+                animalsIntersect1.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nИмена, которые есть есть в обоих коллекциях:");
+            if (animalsIntersect2.Count != 0)
+                animalsIntersect2.ForEach(a => Console.WriteLine(a));
+            else
+                Console.WriteLine("Элементы не найдены");
+
+            Console.WriteLine("\nИспользование операций над множествами успешно завершено");
+        }
+
+        private static void DataAggregation()
+        {
+            Console.WriteLine("\n6 - Агрегирование данных");
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекции пусты");
+                Console.WriteLine("Агрегирование данных не завершено");
+                return;
+            }
+
+            Console.WriteLine("Найдем средний вес элементов коллекции");
+            Console.WriteLine("Если средний вес равен 0, значит элементов нет в коллекции");
+
+            var averageWeight1 = Query.AverageWeight(list);
+            var averageWeight2 = Methods.AverageWeight(list);
+
+            Console.WriteLine($"Средний вес элементов коллекции равен: {averageWeight1}");
+            Console.WriteLine($"Средний вес элементов коллекции равен: {averageWeight2}");
+
+            Console.WriteLine("\nАгрегирование данных успешно завершено");
+        }
+
+        private static void DataGrouping()
+        {
+            Console.WriteLine("\n7 - Группировка данных");
+
+            if (list.Count == 0)
+            {
+                Console.WriteLine("Коллекции пусты");
+                Console.WriteLine("Группировка данных не завершена");
+                return;
+            }
+
+            var animalsGroup1 = Query.GroupAnimals(list);
+            var animalsGroup2 = Methods.GroupAnimals(list);
+
+            if (animalsGroup1.Count != 0)
+            {
+                Console.WriteLine("Сгруппированные коллекции по имени:");
+
+                animalsGroup1.ForEach(animals =>
+                {
+                    Console.WriteLine(animals.Key);
+                    animals.ToList().ForEach(e => Console.WriteLine(e));
+                    Console.WriteLine();
+                });
+            }
+            else
+            {
+                Console.WriteLine("Список пуст");
+            }
+
+            if (animalsGroup2.Count != 0)
+            {
+                Console.WriteLine("Сгруппированные коллекции по имени:");
+
+                animalsGroup2.ForEach(animals =>
+                {
+                    Console.WriteLine(animals.Key);
+                    animals.ToList().ForEach(e => Console.WriteLine(e));
+                    Console.WriteLine();
+                });
+            }
+            else
+            {
+                Console.WriteLine("Список пуст");
+            }
+
+            Console.WriteLine("\nГруппировка данных успешно завершена");
+        }
+
+        public static class Query
+        {
+            public static List<Animals> SearchAnimals(List<List<Animals>> list, string name)
+            {
+                return (from animal in GeneralList(list) where animal.Name == name select animal).ToList();
+            }
+
+            public static int CountAnimals(List<List<Animals>> list, string name)
+            {
+                return (from animal in GeneralList(list) where animal.Name == name select animal).Count();
+            }
+
+            public static List<string> ExpectAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : (from animal in list[0] select animal.Name).Except(from animal in list[1] select animal.Name).ToList();
+            }
+
+            public static List<Animals> UnionAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : (from animal in list[0] select animal).Union(from animal in list[1] select animal).ToList();
+            }
+
+            public static List<string> IntersectAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : (from animal in list[0] select animal.Name).Intersect(from animal in list[1] select animal.Name).ToList();
+            }
+
+            public static double AverageWeight(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? 0 : (from animal in GeneralList(list) select animal.Weight).Average();
+            }
+
+            public static List<IGrouping<string, Animals>> GroupAnimals(List<List<Animals>> list)
+            {
+                return (from animal in GeneralList(list) group animal by animal.Name).ToList();
+            }
+        }
+
+        public static class Methods
+        {
+            public static List<Animals> SearchAnimals(List<List<Animals>> list, string name)
+            {
+                return GeneralList(list).Where(animal => animal.Name == name).ToList();
+            }
+
+            public static int CountAnimals(List<List<Animals>> list, string name)
+            {
+                return GeneralList(list).Count(animal => animal.Name == name);
+            }
+
+            public static List<string> ExpectAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : list[0].Select(animal => animal.Name).Except(list[1].Select(animal => animal.Name)).ToList();
+            }
+
+            public static List<Animals> UnionAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : list[0].Union(list[1]).ToList();
+            }
+
+            public static List<string> IntersectAnimals(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? default : list[0].Select(animal => animal.Name).Intersect(list[1].Select(animal => animal.Name)).ToList();
+            }
+
+            public static double AverageWeight(List<List<Animals>> list)
+            {
+                return list.Count == 0 ? 0 : GeneralList(list).Average(animal => animal.Weight);
+            }
+
+            public static List<IGrouping<string, Animals>> GroupAnimals(List<List<Animals>> list)
+            {
+                return GeneralList(list).GroupBy(animal => animal.Name).ToList();
+            }
+        }
+
+        public static List<Animals> GeneralList(List<List<Animals>> list)
+        {
+            return list.Aggregate(new List<Animals>(), (newList, el) => newList.Union(el).ToList());
+        }
+
+        private static int InputInt()
+        {
+            int number;
+            bool check;
+            do
+            {
+                Console.Write("Ввод: ");
+                check = int.TryParse(Console.ReadLine(), out number) && number >= INT_MIN_VALUE && number <= INT_MAX_VALUE;
+                if (!check) Console.WriteLine("Ошибка! Введите целое число пределах от {0} до {1} (включительно)", INT_MIN_VALUE, INT_MAX_VALUE);
+            } while (!check);
+            return number;
+        }
+
+        private static uint InputUint()
+        {
+            uint number;
+            bool check;
+            do
+            {
+                Console.Write("Ввод: ");
+                check = uint.TryParse(Console.ReadLine(), out number) && number >= UINT_MIN_VALUE && number <= UINT_MAX_VALUE;
+                if (!check) Console.WriteLine("Ошибка! Введите целое число пределах от {0} до {1} (включительно)", UINT_MIN_VALUE, UINT_MAX_VALUE);
+            } while (!check);
+
+            return number;
+        }
+
+        private static string InputString()
+        {
+            string input;
+            bool check;
+            do
+            {
+                Console.Write("Ввод: ");
+                input = Console.ReadLine();
+                check = input.Trim() != "";
+                if (!check) Console.WriteLine("Ошибка! Введите не пустые данные");
+            } while (!check);
+            return input;
+        }
+    }
+}
